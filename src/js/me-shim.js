5684d14fde24aa0d31b4fff1628969bbccac3739
Alexander Bihary <alex.bihary@easy2.com>
2012-08-22 16:21:10 -0400

    update js files to 2.9.1

--- src/js/me-shim.js
+++ src/js/me-shim.js
@@ -82,6 +82,7 @@ Default options
 mejs.MediaElementDefaults = {
 	// allows testing on HTML5, flash, silverlight
 	// auto: attempts to detect what the browser can do
+	// auto_plugin: prefer plugins and then attempt native HTML5
 	// native: forces HTML5 playback
 	// shim: disallows HTML5, will attempt either Flash or Silverlight
 	// none: forces fallback view
@@ -251,7 +252,7 @@ mejs.HtmlMediaElementShim = {
 		
 
 		// test for native playback first
-		if (supportsMediaTag && (options.mode === 'auto' || options.mode === 'native')) {
+		if (supportsMediaTag && (options.mode === 'auto' || options.mode === 'auto_plugin' || options.mode === 'native')) {
 						
 			if (!isMediaTag) {
 
@@ -280,12 +281,15 @@ mejs.HtmlMediaElementShim = {
 					htmlMediaElement.src = result.url;
 				}
 			
-				return result;
+				// if `auto_plugin` mode, then cache the native result but try plugins.
+				if (options.mode !== 'auto_plugin') {
+					return result;
+				}
 			}
 		}
 
 		// if native playback didn't work, then test plugins
-		if (options.mode === 'auto' || options.mode === 'shim') {
+		if (options.mode === 'auto' || options.mode === 'auto_plugin' || options.mode === 'shim') {
 			for (i=0; i<mediaFiles.length; i++) {
 				type = mediaFiles[i].type;
 
@@ -322,6 +326,12 @@ mejs.HtmlMediaElementShim = {
 			}
 		}
 		
+		// at this point, being in 'auto_plugin' mode implies that we tried plugins but failed.
+		// if we have native support then return that.
+		if (options.mode === 'auto_plugin' && result.method === 'native') {
+			return result;
+		}
+
 		// what if there's nothing to play? just grab the first available
 		if (result.method === '' && mediaFiles.length > 0) {
 			result.url = mediaFiles[0].url;
@@ -332,6 +342,7 @@ mejs.HtmlMediaElementShim = {
 
 	formatType: function(url, type) {
 		var ext;
+
 		// if no type is supplied, fake it with the extension
 		if (url && !type) {		
 			return this.getTypeFromFile(url);
@@ -352,8 +363,23 @@ mejs.HtmlMediaElementShim = {
 		var ext = url.substring(url.lastIndexOf('.') + 1);
 		if(ext && ext.toLowerCase() == "swf") {
 			return 'application/x-shockwave-flash';
-		}
-		return (/(mp4|m4v|ogg|ogv|webm|flv|wmv|mpeg|mov)/gi.test(ext) ? 'video' : 'audio') + '/' + ext;
+		return (/(mp4|m4v|ogg|ogv|webm|webmv|flv|wmv|mpeg|mov)/gi.test(ext) ? 'video' : 'audio') + '/' + this.getTypeFromExtension(ext);
+	},
+	
+	getTypeFromExtension: function(ext) {
+		var ext_types = {
+			'mp4': 'mp4,m4v',
+			'ogg': 'ogg,ogv,oga',
+			'webm': 'webm,webmv,webma'
+		};
+		var r = ext;
+		mejs.$.each(ext_types, function(key, value) {
+			if (value.indexOf(ext) > -1) {
+				r = key;
+				return;
+			}
+		});
+		return r;
 	},
 
 	createErrorMessage: function(playback, options, poster) {
@@ -491,31 +517,33 @@ mejs.HtmlMediaElementShim = {
 					specialIEContainer = document.createElement('div');
 					container.appendChild(specialIEContainer);
 					specialIEContainer.outerHTML =
-					'<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="//download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab" ' +
-					'id="' + pluginid + '" width="' + width + '" height="' + height + '">' +
+'<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="//download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab" ' +
+'id="' + pluginid + '" width="' + width + '" height="' + height + '">' +
 					'<param name="movie" value="' + options.pluginPath + flashSwfToEmbed + '?x=' + (new Date()) + '" />' +
-					'<param name="flashvars" value="' + initVars.join('&amp;') + '" />' +
-					'<param name="quality" value="high" />' +
+'<param name="flashvars" value="' + initVars.join('&amp;') + '" />' +
+'<param name="quality" value="high" />' +
 					'<param name="bgcolor" value="#FFFFFF" />' +
 					'<param name="wmode" value="'+options.ieFlashWindowMode+'" />' +
-					'<param name="allowScriptAccess" value="always" />' +
-					'<param name="allowFullScreen" value="true" />' +
-					'</object>';
+'<param name="allowScriptAccess" value="always" />' +
+'<param name="allowFullScreen" value="true" />' +
+'</object>';
+
 				} else {
+
 					container.innerHTML =
-					'<embed id="' + pluginid + '" name="' + pluginid + '" ' +
-					'play="true" ' +
-					'loop="false" ' +
-					'quality="high" ' +
+'<embed id="' + pluginid + '" name="' + pluginid + '" ' +
+'play="true" ' +
+'loop="false" ' +
+'quality="high" ' +
 					'bgcolor="#FFFFFF" ' +
-					'wmode="transparent" ' +
-					'allowScriptAccess="always" ' +
-					'allowFullScreen="true" ' +
-					'type="application/x-shockwave-flash" pluginspage="//www.macromedia.com/go/getflashplayer" ' +
+'wmode="transparent" ' +
+'allowScriptAccess="always" ' +
+'allowFullScreen="true" ' +
+'type="application/x-shockwave-flash" pluginspage="//www.macromedia.com/go/getflashplayer" ' +
 					'src="' + options.pluginPath + flashSwfToEmbed + '" ' +
-					'flashvars="' + initVars.join('&') + '" ' +
-					'width="' + width + '" ' +
-					'height="' + height + '"></embed>';
+'flashvars="' + initVars.join('&') + '" ' +
+'width="' + width + '" ' +
+'height="' + height + '"></embed>';
 				}
 				break;
 			
@@ -544,7 +572,7 @@ mejs.HtmlMediaElementShim = {
 			
 			// DEMO Code. Does NOT work.
 			case 'vimeo':
-				console.log('vimeoid');
+				//console.log('vimeoid');
 				
 				pluginMediaElement.vimeoid = playback.url.substr(playback.url.lastIndexOf('/')+1);
 				
